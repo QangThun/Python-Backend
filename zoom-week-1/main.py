1bc8c2feee1edb45529120467f2a7cf981a53110
QangThun <phamquangthuan3107@gmail.com>
2025-10-13 23:27:42 +0700

    update day1

--- zoom-week-1/main.py
+++ zoom-week-1/main.py
@@ -3,15 +3,15 @@
 # b =input("Nhập vào số b: ")
 # print("Tổng của a và b là: ", int(a)+int(b))
 
-# 1. Print: in dữ liệu ra màn hình
+# s1.Print: in dữ liệu ra màn hình
 # print('Xin', 'chào', sep='-', end='-') #sep: dấu phân cách, end: kết thúc bằng gì
 # print('bạn')
 
-# 2. Input: nhập dữ liệu từ bàn phím
+# s2.Input: nhập dữ liệu từ bàn phím
 # name = input("Nhập vào tên của bạn: ")
 # print("xin chào" + f"{name}") 
 
-# 3. Biến
+# s3.Biến số (variable) là một khái niệm cơ bản trong lập trình, biến dùng để lưu trữ thông tin, các tham chiếu và sử dụng để thao tác dữ liệu
 # a = 5 #kiểu int
 # b = 5.5 #kiểu float
 # c = "Hello" #kiểu str
@@ -22,12 +22,12 @@
 # x = y = z = 1 #gán nhiều biến cùng giá trị
 # # print(x, y, z)
 
-# 4. Hằng số
+# s4.Hằng số
 # import math
 # print(math.pi) #hằng số pi
 # # PI = 3.14 #khai báo hằng số
 
-# 5. Kiểu dữ liệu
+# s5.Kiểu dữ liệu
 # a = 5 #int
 # b = 5.5 #float
 # c = "Hello" #str
@@ -42,7 +42,7 @@
 # print(type(e))
 # print(type(f))
 
-# 6. Ép kiểu dữ liệu
+# s6.Ép kiểu dữ liệu
 
 # int() - chuyển đổi sang kiểu số nguyên
 # float() - chuyển đổi sang kiểu số thực
@@ -53,7 +53,7 @@
 # m = "200"
 # print(n+int(m)) #ép kiểu m từ str sang int để cộng với n , nếu ko báo lỗi
 
-#7. Cac toán số học 
+# s7.Các phép toán số học 
 # +, -, *, /, //, %, **
 
 # print(a/b) #chia lấy kết quả thực
@@ -70,7 +70,7 @@
 # print("{0} % {1} = {2}" .format(a ,b , a%b))    
 # print("{0} ** {1} = {2}" .format(a ,b , a**b))    
 
-# 8. Toán tử so sánh
+# s8.Toán tử so sánh
 # ==, !=, >, <, >=, <= 
 
 # print(a != b) #so sánh a có khác b 
@@ -84,7 +84,7 @@
 # print(x >= y)
 # print(x <= y)
 
-# 9. Toán tử logic
+# s9.Toán tử logic
 # and, or, not
 
 # x = int(input("Nhập vào x: "))
@@ -93,7 +93,7 @@
 # print ( x > 0 or y > 5)
 # print ( not ( x > y ))
 
-# 10. Toán tử gán
+# s10. Toán tử gán
 # =, +=, -=, *=, /=, //=, %=, **=
 
 # a = 5
@@ -112,7 +112,7 @@
 # a **= 2 # a = a ** 2
 # print(int(a))
 
-# 11.Thư viện math  
+# s11.Thư viện math  
 import math 
 # print(math.pi) #hằng số pi
 # print(math.e) #hằng số e
@@ -140,13 +140,13 @@ import math
 # print("pow(x , 2) = ", math.pow(x, 2))
 # print("ceil(x) = ", math.ceil(x))
 
-# 13.Toán tử ba ngôi
+# s12.Toán tử ba ngôi
 # điều kiện đúng if điều kiện else điều kiện sai
 # a = int(input("Nhập vào a: "))
 # kq = " số chẵn " if ( a % 2 == 0) else " số lẻ "
 # print ( a , "là " , kq) 
 
-#14.Câu lệnh rẽ nhánh if...else
+# s13.Câu lệnh rẽ nhánh if...else
 # x = float(input("Nhập vào x: "))
 # vd1
 # if x % 2 == 0 :
@@ -167,24 +167,67 @@ import math
 #     print("Học sinh yếu")
 # print("Kết thúc chương trình")
 
-# 15.Giải phương trình bậc 2
-print("Giải phương trình bậc 2: ax^2 + bx + c = 0")
-a = float(input("Nhập vào a: "))
-b = float(input("Nhập vào b: "))
-c = float(input("Nhập vào c: "))
-if (a != 0): 
-    delta = b**2 - 4*a*c
-    if (delta < 0 ):
-        print("Phương trình vô nghiệm")
-    elif (delta == 0) :
-        x = -b/(2*a)
-        print("Phương trình có nghiệm kép x1 = x2 = ", x) 
-    else :
-        x1 = (-b + math.sqrt(delta)) / (2*a)
-        x2 = (-b - math.sqrt(delta)) / (2*a)
-        print("Phương trình có 2 nghiệm phân biệt: x1 = ", x1, " x2 = ", x2)
-else :
-    print("Không phải phương trình bậc 2")
+# s14.Giải phương trình bậc 2
+# print("Giải phương trình bậc 2: ax^2 + bx + c = 0")
+# a = float(input("Nhập vào a: "))
+# b = float(input("Nhập vào b: "))
+# c = float(input("Nhập vào c: "))
+# if (a != 0): 
+#     delta = b**2 - 4*a*c
+#     if (delta < 0 ):
+#         print("Phương trình vô nghiệm")
+#     elif (delta == 0) :
+#         x = -b/(2*a)
+#         print("Phương trình có nghiệm kép x1 = x2 = ", x) 
+#     else :
+#         x1 = (-b + math.sqrt(delta)) / (2*a)
+#         x2 = (-b - math.sqrt(delta)) / (2*a)
+#         print("Phương trình có 2 nghiệm phân biệt: x1 = ", x1, " x2 = ", x2)
+# else :
+#     print("Không phải phương trình bậc 2")
+
+# s15.Kiểu dữ liệu List 
+colors = ["red", "green" , "orange"]
+
+# s15.1.Phương thức thao tác trên List
+# append(): Thêm một phần tử vào cuối List.
+# extend(): Thêm nhiều phần tử từ một List khác vào cuối List hiện tại.
+# insert(): Thêm một phần tử vào vị trí cụ thể trong List.
+# remove(): Xóa phần tử đầu tiên khớp với giá trị được chỉ định.
+# pop(): Xóa và trả về phần tử tại một vị trí xác định (hoặc phần tử cuối).
+# clear(): Xóa toàn bộ phần tử trong List.
+# index(): Trả về chỉ số của phần tử đầu tiên khớp với giá trị được chỉ định.
+# count(): Đếm số lần một phần tử xuất hiện trong List.
+# reverse(): Đảo ngược thứ tự các phần tử trong List.
+# copy(): Trả về một bản sao của List.
+# sort(): Sắp xếp các phần tử trong List tại chỗ.
+
+# s15.2.Hàm thao tác với list trong Python
+# len(): Trả về số lượng phần tử trong List.
+# min(): Trả về phần tử nhỏ nhất trong List.
+# max(): Trả về phần tử lớn nhất trong List.
+# sum(): Tính tổng các phần tử trong List (dành cho List chứa các số).
+# sorted(): Trả về một List mới đã được sắp xếp mà không thay đổi List ban đầu.
+
+# s15.3.Các hàm toán học và logic với List
+# any(): Trả về True nếu có bất kỳ phần tử nào trong List là True.
+# all(): Trả về True nếu tất cả các phần tử trong List đều là True.
+
+#Truy xuất dữ liệu index , (star:end) 
+print(colors[2])
+print(colors[0:2])
+#Thêm phân tử vào vị trí cuối append
+colors.append("blue")
+print(colors)
+#Thêm phần tử vào vị trí được chỉ định insert(index,x)
+colors.insert(1 , "white")
+print(colors)
+
+
+
+
+
+
 
 
 
--- zoom-week-1/note.md
+++ zoom-week-1/note.md
@@ -1,1 +1,15 @@
-# Những kiến thức đã học
+# Danh mục những kiến thức đã học
+
+s1.Print: in dữ liệu ra màn hình
+s2.Input: nhập dữ liệu từ bàn phím
+s3.Biến số
+s4.Hằng số
+s5.Kiểu dữ liệu 6. Ép kiểu dữ liệu
+s7.Các phép toán số học
+s8.Toán tử so sánh
+s9.Toán tử logic 10. Toán tử gán
+s11.Thư viện math
+s12.Toán tử ba ngôi
+s13.Câu lệnh rẽ nhánh if...else
+s14.Giải phương trình bậc 2
+s15.Kiểu dữ liệu List
