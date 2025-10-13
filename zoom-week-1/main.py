8c9519205d2c4afd1cf3ef5d5f0f82e15778526d
QangThun <phamquangthuan3107@gmail.com>
2025-10-13 16:10:26 +0700

    First commit - Python ToLearn

---
+++ zoom-week-1/main.py
@@ -0,0 +1,48 @@
+# print ("Hello, World!")
+# a =input("Nhập vào số a: ")
+# b =input("Nhập vào số b: ")
+# print("Tổng của a và b là: ", int(a)+int(b))
+
+# 1. Print: in dữ liệu ra màn hình
+# print('Xin', 'chào', sep='-', end='-') #sep: dấu phân cách, end: kết thúc bằng gì
+# print('bạn')
+
+# 2. Input: nhập dữ liệu từ bàn phím
+# name = input("Nhập vào tên của bạn: ")
+# print("xin chào" + f"{name}") 
+
+# 3. Biến
+# a = 5 #kiểu int
+# b = 5.5 #kiểu float
+# c = "Hello" #kiểu str
+
+# x, y, z = 1, 2, 3 #khai báo nhiều biến cùng lúc
+# print(x, y, z)
+
+# x = y = z = 1 #gán nhiều biến cùng giá trị
+# # print(x, y, z)
+
+# 4. Hằng số
+# import math
+# print(math.pi) #hằng số pi
+# # PI = 3.14 #khai báo hằng số
+
+# 5. Kiểu dữ liệu
+# a = 5 #int
+# b = 5.5 #float
+# c = "Hello" #str
+# d = True #bool    
+# e = None #kiểu dữ liệu rỗng
+# f = 1 + 2j #kiểu dữ liệu phức
+# print(type(a)) #kiểm tra kiểu dữ liệu
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
+
+# 6. Ép kiểu dữ liệu
+# int() - chuyển đổi sang kiểu số nguyên
+# float() - chuyển đổi sang kiểu số thực
+# str() - chuyển đổi sang kiểu chuỗi
+# bool() - chuyển đổi sang kiểu boolean
