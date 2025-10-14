3b2ced86a88d1f01bc58cbf706c039b246c17467
QangThun <phamquangthuan3107@gmail.com>
2025-10-14 22:17:10 +0700

    day2

--- 01.Biến và kiểu dữ liệu/main.py
+++ 01.Biến và kiểu dữ liệu/main.py
@@ -53,22 +53,6 @@
 # m = "200"
 # print(n+int(m)) #ép kiểu m từ str sang int để cộng với n , nếu ko báo lỗi
 
-# s7.Các phép toán số học 
-# +, -, *, /, //, %, **
-
-# print(a/b) #chia lấy kết quả thực
-# print(a//b) #chia lấy kết quả nguyên  
-# print(a%b) #chia lấy phần dư
-# print(a**b) #lũy thừa
-# a = int(input("Nhập vào a: "))
-# b = int(input("Nhập vào b: "))
-# print("{0} + {1} = {2}" .format(a ,b , a+b))    
-# print("{0} - {1} = {2}" .format(a ,b , a-b))    
-# print("{0} * {1} = {2}" .format(a ,b , a*b))    
-# print("{0} / {1} = {2}" .format(a ,b , a/b))    
-# print("{0} // {1} = {2}" .format(a ,b , a//b))    
-# print("{0} % {1} = {2}" .format(a ,b , a%b))    
-# print("{0} ** {1} = {2}" .format(a ,b , a**b))    
 
 # s8.Toán tử so sánh
 # ==, !=, >, <, >=, <= 
---
+++ 01.Biến và kiểu dữ liệu/note.md
@@ -0,0 +1,8 @@
+# Danh mục những kiến thức đã học
+
+s1.Print: in dữ liệu ra màn hình
+s2.Input: nhập dữ liệu từ bàn phím
+s3.Biến số
+s4.Hằng số
+s5.Kiểu dữ liệu
+s6.Ép kiểu dữ liệu
