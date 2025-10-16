3d4df942148421966cedcf17a77082d0bb46f4be
QangThun <phamquangthuan3107@gmail.com>
2025-10-16 21:59:13 +0700

    day4

--- 02.Các phép toán số học/main.py
+++ 02.Các phép toán số học/main.py
@@ -7,7 +7,7 @@
 # print(a**b) #lũy thừa
 a = int(input("Nhập vào a: "))
 b = int(input("Nhập vào b: "))
-print("{0} + {1} = {2}" .format(a ,b , a+b))    
+print("{0} + {1} = {2}" .format(a ,b , a+b))
 print("{0} - {1} = {2}" .format(a ,b , a-b))    
 print("{0} * {1} = {2}" .format(a ,b , a*b))    
 print("{0} / {1} = {2}" .format(a ,b , a/b))    
