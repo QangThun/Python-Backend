b07894c29f5d18d31639bcc0ce7e7847602332e7
QangThun <phamquangthuan3107@gmail.com>
2025-10-19 22:11:22 +0700

    day5

--- 05.Kiểu dữ liệu List/main.py
+++ 05.Kiểu dữ liệu List/main.py
@@ -1,2 +1,15 @@
-#  abc
-print("hello")
+# tạo list
+my_list = [ 12 , 3.5 , "string" , [5,7]]
+print("my_list")
+print(type(my_list))
+
+# tạo list trống
+empty_lst = list()
+print(empty_lst)
+
+# lấy ra phần tử list 
+print(len(my_list))
+
+# truy xuất dữ liệu 
+print(my_list)
+
