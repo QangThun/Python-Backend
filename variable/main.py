273e1c3de4712e3c66c47f7d0522e9894c9c645e
QangThun <phamquangthuan3107@gmail.com>
2025-11-06 16:15:10 +0700

    abc

---
+++ variable/main.py
@@ -0,0 +1,26 @@
+# Variables = A container for a value (string, number, boolean, float,etc.)
+            # A variable behaves as the value that it contains
+
+# String Variable
+first_name = "John"
+print("Hello " + first_name + "!")
+print(f"Hello {first_name}!")
+print("Hello {}".format(first_name))
+
+# Integer Variable
+age = 25
+print("You are " + str(age) + " years old.")    
+print(f"You are {age} years old.")
+print("You are {} years old.".format(age))
+
+# Float Variable
+height = 5.9    
+print("Your height is " + str(height) + " feet.")    
+print(f"Your height is {height} feet.")
+print("Your height is {} feet.".format(height))
+
+# Boolean Variable
+is_student = True
+print("Are you a student? " + str(is_student))    
+print(f"Are you a student? {is_student}")   
+print("Are you a student? {}".format(is_student))
